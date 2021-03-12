ccb5a666c282d78e0aa08a56dcdecaca8dbab494
salkorad <salko.radovovic@gmail.com>
2021-03-12 01:41:45 +0100

    Dodao zadatak sa stringovima

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    var myFirstName = "Salko";
+    var myLastName = "Radovovic";
+    var myHomeTown = "Sarajevo";
+
+    return {
+        
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
